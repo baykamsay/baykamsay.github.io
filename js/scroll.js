0790d8f57c2e6fd83653911bb83cfe404de03eaa
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-02-29 10:31:52 +0300

    Modified navbar
    Added a shadow and a background when user starts to scroll.

---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+// learned from https://css-tricks.com/styling-based-on-scroll-position/
+
+const debounce = (fn) => {
+  
+  let frame;
+
+  return (...params) => {
+    
+    if (frame) { 
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+
+  } 
+};
+
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+}
+document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+storeScroll();
+
